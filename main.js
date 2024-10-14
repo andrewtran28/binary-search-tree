ad56f3ec564f4855ce1ec2b5f9819582d5021c95
andrewtran28 <tran.andrew@outlook.com>
2024-10-14 12:17:21 -0400

    implement insert and delete function.

--- main.js
+++ main.js
@@ -9,13 +9,19 @@ class Node {
 }
 
 class Tree {
-    initialBuildTree(array) {
+    constructor() {
+        this.array = [];
+        this.root = null;
+    };
+
+    buildTree(array) {
         let start = 0;
         let end = array.length - 1;
-        return this.buildTree(array, start, end);
+        this.root = this.createBST(array, start, end);
+        return this.prettyPrint (this.root);
     }
 
-    buildTree(array, start, end) {
+    createBST(array, start, end) {
         if (start > end) {
             return null;
         }
@@ -23,26 +29,70 @@ class Tree {
         let mid = start + Math.floor((end - start) / 2);
         let root = new Node(array[mid]);
 
-        root.left = this.buildTree(array, start, mid-1);
-        root.right = this.buildTree(array, mid + 1, end);
+        root.left = this.createBST(array, start, mid-1);
+        root.right = this.createBST(array, mid + 1, end);
 
         this.prettyPrint(root);
+        return root;
+    }
+
+    insert(root, key) {
+        if (root === null) {
+            return new Node(key);
+        }
+        
+        if (root.data === key) {
+            return root;
+        }
+
+        if (key < root.data) {
+            root.left = this.insert(root.left, key);
+        } else if (key > root.data) {
+            root.right = this.insert(root.right, key);
+        }
 
         return root;
     }
 
-    removeDuplicates(array) {
-        for (var i = 0; i < array.length; i++) {
-            if (array[i] === array [i-1]) {
-                array.splice(i, 1);
-            }
+    delete(root, key) {
+        if (root === null) {
+            return root;
         }
-        return array;
+
+        if (root.data > key) {
+            root.left = this.delete(root.left, key);
+        } else if (root.data < key) {
+            root.right = this.delete(root.right, key);
+        } else {
+            if (root.left === null) {
+                return root.left;
+            }
+        
+            if (root.right === null) {
+                return root.right;
+            }
+
+            let successor = this.getSuccessor(root);
+            root.data = successor.data;
+            root.right = this.delete(root.right, successor.key);
+        }
+
+        return root;
     }
 
-    processData(array) {
-        let processedArray = this.removeDuplicates(mergeSort(array));
-        return processedArray;
+    getSuccessor(root) {
+        let curr = root.right;
+        while(curr && curr.left){
+          curr = curr.left;
+        }
+        return curr;
+    }
+
+    preOrder(root) {
+        if (root === null) return;
+        console.log(root.data);
+        this.preOrder(root.left);
+        this.preOrder(root.right);
     }
 
     prettyPrint (node, prefix = "", isLeft = true) {
@@ -58,24 +108,6 @@ class Tree {
         }
     }
 
-    insert(root, value) {
-        if (root === null) {
-            return new Node (value);
-        }
-
-        if (root.value === value) {
-            return root;
-        }
-
-        if (value < root.value) {
-            root.left = insert(root.left, value);
-        } else if (value > root.value) {
-            root.right = insert(root.right, value);
-        }
-
-        return root;
-    }
-
     inOrder(root) {
         if (root !== null) {
             inOrder(root.left);
@@ -85,12 +117,34 @@ class Tree {
     }
 }
 
+const processArr = (function () {
+    const sort = (array) => {
+        let processedArray = removeDuplicates(mergeSort(array));
+        return processedArray;
+    }
+
+    const removeDuplicates = (array) => {
+        for (var i = 0; i < array.length; i++) {
+            if (array[i] === array [i-1]) {
+                array.splice(i, 1);
+            }
+        }
+        return array;
+    }
+
+    return { sort };
+})();
 
 let testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let tree = new Tree();
+let arr = processArr.sort(testArray);
+console.log(arr);
 
-
-let test = new Tree();
-test.insert(12);
-let data = test.processData(testArray);
-console.log(data);
-test.initialBuildTree(data);
+tree.buildTree(arr);
+tree.insert(tree.root, 12);
+tree.prettyPrint(tree.root);
+tree.delete(tree.root, 12);
+tree.prettyPrint(tree.root);
+// tree.prettyPrint(tree.root);
+// tree.buildTree(arr);
+// console.log(tree.array);
